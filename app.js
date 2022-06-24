a4081801ffcf8944d9ae48e4fa4e2a2aafd06105
unknown <mayankraman1900@gmail.com>
2022-06-24 22:46:38 +0530

    Phase 3 completed(exports)

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const date = require(__dirname + "/date.js")
+
+
 
 const app=express();
-let items =["Buy Food","Cook Food","Eat Food"];
+const items =["Buy Food","Cook Food","Eat Food"];
+const workItems = [];
 app.set('view engine','ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -10,26 +14,36 @@ app.use(express.static("public"));
 
 app.get("/",function(req,res){
 
- let today=new Date();
+  const day = date.getDate();
 
- let options={
-   weekday:"long",
-    day:"numeric",
-    month:"long"
- };
-
- let day=today.toLocaleDateString("en-US",options);
-
-  res.render("list",({kind0fDay:day,newListItems: items}));
+  res.render("list",({listTitle:day,newListItems: items}));
 });
+
+app.get("/work",function(req,res)
+{
+  res.render("list",{listTitle: "Work List", newListItems:workItems})
+})
+
 
 app.post("/",function(req,res){
- const item=req.body.newItem;
-
- items.push(item);
-
-  res.redirect("/");
+ let item=req.body.newItem;
+if(req.body.list === "Work")
+{
+  workItems.push(item);
+  res.redirect("/work");
+}
+else{
+  items.push(item);
+   res.redirect("/");
+}
 });
+
+app.post("/work",function(req,res)
+{
+  let item=req.body.newItem;
+workItems.push(item);
+res.redirect("/work");
+})
 
 app.listen(3000,function()
 {
