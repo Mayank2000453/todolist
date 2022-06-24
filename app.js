5bfc17785f0acde3d37b91b55951718182432ec6
unknown <mayankraman1900@gmail.com>
2022-06-24 21:38:22 +0530

    Phase 1 completed

---
+++ app.js
@@ -0,0 +1,36 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+let items =["Buy Food","Cook Food","Eat Food"];
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+
+ let today=new Date();
+
+ let options={
+   weekday:"long",
+    day:"numeric",
+    month:"long"
+ };
+
+ let day=today.toLocaleDateString("en-US",options);
+
+  res.render("list",({kind0fDay:day,newListItems: items}));
+});
+
+app.post("/",function(req,res){
+ const item=req.body.newItem;
+
+ items.push(item);
+
+  res.redirect("/");
+});
+
+app.listen(3000,function()
+{
+  console.log("Server staretd on port 3000");
+})
